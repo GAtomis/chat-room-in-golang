c5733f394674df9cf2c4a2cdba7ec4c98bda9d84
GAtomis <850680822@qq.com>
2023-07-20 17:24:07 +0800

    init : project

---
+++ models/UserBasic.go
@@ -0,0 +1,23 @@
+package models
+
+import "github.com/jinzhu/gorm"
+
+type UserBasic struct {
+	gorm.Model
+	Name          string
+	PassWord      string
+	Phone         string
+	Email         string
+	Identity      string
+	ClientIP      string
+	ClientPort    string
+	LoginTime     uint64
+	HeartbeatTime uint64
+	LogoutTime    uint64
+	IsLogout      bool
+	DeviceInfo    string
+}
+
+func (table *UserBasic) TableName() string {
+	return "user_basic"
+}
